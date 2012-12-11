1764419bca1d05fee02ca5b9e3e62d814fc8aace
mateuszjancy <mateusz.jancy@gmail.com>
2012-12-11 21:49:57 +0100

    added mongoose

---
+++ domain/DataProvider.js
@@ -0,0 +1,41 @@
+//Tutorial on 
+//->http://addyosmani.github.com/backbone-fundamentals/
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+
+var Page = new Schema({
+  URL: String,
+  h1:String,
+  p:String,
+  img:String,
+  order: Number,
+  large: Boolean
+});
+
+var Link = new Schema({
+		label:String, 
+		URL: String,
+		order: Number
+});
+
+Page.statics.findByUrl = function (url, callback) {
+  return this.find({ URL: url}, callback);
+}
+
+var Link = mongoose.model("Link", Link);
+var Page = mongoose.model("Page", Page);
+
+//Connection
+mongoose.connect('mongodb://localhost/my_database');
+
+//UseCase
+var link = new Link({label:"About", URL:"about", order: 1});
+console.log(link.label);
+link.save();
+
+var page = new Page({URL:"about", h1:"test h1"});
+page.save();
+Page.findByUrl(link.URL, function(err, pages){
+	if(!err) console.log(pages);
+});
