0050299b8f749bbdc45520149b6e1bc6d00e7dc4
mateuszjancy <mateusz.jancy@gmail.com>
2012-12-24 15:47:11 +0100

    Backbone

--- public/js/routers/router.js
+++ public/js/routers/router.js
@@ -1,10 +1,12 @@
 //http://stackoverflow.com/questions/6262444/get-collection-id-in-backbone-without-loading-the-entire-collection
+//http://coenraets.org/blog/2011/12/backbone-js-wine-cellar-tutorial-part-1-getting-started/
 var app = app || {};
 
 app.AppRouter = Backbone.Router.extend({
 	routes: {
 		"":"list",
-		"page/:id":"pageDetails"
+		"page/:id":"pageDetails",
+		"create/page":"createPage"
 	},
 
 	list: function(){
@@ -17,8 +19,13 @@ app.AppRouter = Backbone.Router.extend({
 	pageDetails: function(pUrl){
 		app.cmsPageListView  = new app.CmsPageListView({model: app.cmsPageCollection});
 		app.cmsPageCollection.reset();
+		//TODO: cannot find good way for REST
 		app.cmsPageCollection.fetch({data: { id:pUrl } });
 
 		$('#page-container').html(app.cmsPageListView.render().el);
+	}
+
+	createPage: function(){
+
 	}
 });
